b6c35fcc2199a0b346637e1070c11b75d59f444d
Nihal Joshi <nj@localhost.localdomain>
2020-04-22 10:50:56 +0530

    Updates

---
+++ precache-manifest.993a0bd0490ab9a118599a09fbc9e60e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81b11d8e238ebf2c207d7ac4853f7a15",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55d8a37a3f530c780580",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "d9f2ddb9c86a1a165d1c",
+    "url": "/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "55d8a37a3f530c780580",
+    "url": "/static/js/2.bf6807c8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.bf6807c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9f2ddb9c86a1a165d1c",
+    "url": "/static/js/main.dde114fc.chunk.js"
+  },
+  {
+    "revision": "f63ee276b4e0ae54d9e1",
+    "url": "/static/js/runtime-main.5a6e8a30.js"
+  }
+]);
